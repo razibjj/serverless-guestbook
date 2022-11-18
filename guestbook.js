92569c4109a174288e4c9e4029032aff160f5afe
Sina Razi - Git Hub <sina_razi@hotmail.com>
2022-11-18 10:33:50 -0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/sina_razi%40hotmail.com_dev/guestbook/';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
